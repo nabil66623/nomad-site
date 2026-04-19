7380474955200deb99ee9479dc8b325b2acddc21
nabil66623 <nabil@nomadscorp.co>
2026-04-19 18:52:09 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,12 @@
+export const metadata = {
+  title: "Nomad Co.",
+  description: "Operational Consulting & Business Optimization across markets.",
+};
 
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0 }}>{children}</body>
+    </html>
+  );
+}
